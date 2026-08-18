47a3f433337da2753c4712d56909f93893dd4811
Indra Agus Lesmana <indra953@gmail.com>
2026-08-18 10:28:59 +0700

    feat: add logout function

--- lib/screens/guru/teacher_home_screen.dart
+++ lib/screens/guru/teacher_home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class TeacherHomeScreen extends StatefulWidget {
   const TeacherHomeScreen({super.key});
@@ -15,8 +17,19 @@ class _TeacherHomeScreenState extends State<TeacherHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = context.watch<AuthProvider>();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard Guru')),
+      appBar: AppBar(
+        title: const Text('Dashboard Guru'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            tooltip: 'Keluar',
+            onPressed: () => authProvider.logout(),
+          ),
+        ],
+      ),
       body: const Center(
         child: Text(
           'Hello Dashboard',
